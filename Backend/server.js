c05d5255cd331573305351a4e5f6cf96ba1a79e3
unknown <priyanshu.codendev@gmail.com>
2026-08-20 21:41:11 +0530

    Fix server port to use process.env.PORT for Render

--- Backend/server.js
+++ Backend/server.js
@@ -10,6 +10,8 @@ if (!env.jwtSecret) {
 connectToDB()
 
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
